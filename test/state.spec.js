751cd44d2771ff6252ee8c38c6a3fc11a3ca7433
Gideon Farrell <me@gideonfarrell.co.uk>
2014-08-26 01:17:19 +0100

    Added first spec to check creation of State:
    from State.create(props_list)

--- test/state.spec.js
+++ test/state.spec.js
@@ -5,6 +5,11 @@ define(['jasmine/boot', 'state'], function() {
     describe('State.js', function() {
         var State = require('state');
 
-        
+        it('creates state objects from a list of keys', function() {
+            var s = State.create(['hello', 'goodbye']);
+            expect(s.hello).toBeNull();
+            expect(s.goodbye).toBeNull();
+            expect(s.noprop).toBeUndefined();
+        });
     });
 });
